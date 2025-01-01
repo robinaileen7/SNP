3595358095499330513dbe3d88dd34809b5ac65f
robinaileen7 <robinaileen7@g.ucla.edu>
2025-01-01 11:41:31 -0800

    tested fixed-point and ANOVA with real data

--- tool.py
+++ tool.py
@@ -9,28 +9,29 @@ def fixed_pt_iter_BS(E, D, vol):
     tol = 0.00001
     T = 1
     t = 0
-    V_0 = np.ones((1, len(E)))*10000
+    V_0 = E+D
     d_t = (np.log(V_0/D)+((vol**2)/2)*(T-t))/(vol*np.sqrt(T-t))
     V = (E+D*norm.cdf(d_t-vol*np.sqrt(T-t)))/norm.cdf(d_t)
     while np.max(abs(V-V_0)/abs(V))>tol:
         V_0 = V
         d_t = (np.log(V_0 / D) + ((vol ** 2) / 2) * (T - t)) / (vol * np.sqrt(T - t))
         V = (E + D * norm.cdf(d_t - vol * np.sqrt(T - t))) / norm.cdf(d_t)
-    return V
+    return np.array(V)
 
 def anova_2(df):
-    model = ols('y ~ x1 + x2',data=df).fit()
-    print(model.params)
+    # Use ANOVA to gauge idiosyncratic contributions
+    model = ols('TOPIX_TE ~ TMC + Denso + Toyota_Industries + Aisin_Seiki + Toyota_Boshoku + Toyoda_Gosei',data=df).fit()
     anova = anova_lm(model, typ=2)
-    return anova
+    anova_df = anova['sum_sq'].drop('Residual').to_frame()
+    anova_df['TSS'] = anova_df['sum_sq'].sum()
+    anova_df['k'] = anova_df['sum_sq'] / anova_df['TSS']
+    return anova_df['k']
 
 if __name__ == "__main__":
-    E = [100, 120, 125, 140]
-    D = [95, 110, 120, 125]
+    df_ED = pd.read_excel(r'Data\df_ED.xlsx')
+    E = np.array(df_ED['E_t'])
+    D = np.array(df_ED['D_t'])
     print(fixed_pt_iter_BS(E, D, 0.2))
-    df = pd.DataFrame({'y': [11, 15, 17, 17, 20],
-                       'x1': [1, 3, 4, 6, 6],
-                       'x2': [1, 2, 3, 5, 7]
-                       })
-    print(anova_2(df))
+    df_ANOVA = pd.read_excel(r'Data\df_ANOVA.xlsx')
+    print(anova_2(df_ANOVA))
 
