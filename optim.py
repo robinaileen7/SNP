fdee4383921f56af316890f29db09d18c2b47fda
robinaileen7 <robinaileen7@g.ucla.edu>
2025-01-21 21:08:54 -0800

    Added modified Powell

--- optim.py
+++ optim.py
@@ -4,17 +4,20 @@ from scipy.optimize import minimize
 class optim_non_drvt:
     # This class collects optimization methods when f(x) doesn't have a derivative
 
-    def __init__(self, x_0, x_range, my_function):
+    def __init__(self, x_0, x_range, my_function, mat):
         self.x_0 = x_0
         self.x_range = x_range
         self.n = len(x_0)
         self.my_function = my_function
+        np.random.seed(10)
+        # self.mat = np.ones((self.n, self.n))
+        # self.mat = np.maximum(np.random.normal(0, 1, (self.n, self.n)), 0)
+        self.mat = mat
 
-    def golden_ratio(self, a_b, x, which_var):
+    def golden_ratio(self, a_b, x, which_var, tol_gr):
         ratio_1 = 0.618
         ratio_2 = 1 - ratio_1
         iter_gr = 10000
-        tol_gr = 0.001
         ax = a_b[0]
         bx = a_b[1]
         a = min(ax, bx)
@@ -46,9 +49,8 @@ class optim_non_drvt:
                 return 0.5*(x_0+x_1)
         print('Golden Ratio Method Cannot Converge Within the Designated Iteration!')
 
-    def Brent(self, x_0, which_var):
+    def Brent(self, x_0, which_var, tol_Brent, tol_gr):
         iter_Brent = 10000
-        tol_Brent = 0.001
         ax = x_0[which_var][0]
         bx = x_0[which_var][1]
         a = min(ax, bx)
@@ -58,7 +60,7 @@ class optim_non_drvt:
             x_init_0 = x_0.copy()
             x_init_0[which_var]=x_m
             f_x = self.my_function(x_init_0)
-            u = self.golden_ratio([a, b], x_0, which_var)
+            u = self.golden_ratio([a, b], x_0, which_var, tol_gr)
             x_init_u = x_0.copy()
             x_init_u[which_var]=u
             f_u = self.my_function(x_init_u)
@@ -82,14 +84,68 @@ class optim_non_drvt:
         print(which_var)
         print(b-a)
 
-    def min_line_search(self):
+    def min_line_search(self, x_0, tol_Brent, tol_gr):
         out_ls = []
         for i in range(0, self.n):
-            in_ls = self.x_0.copy()
+            in_ls = x_0.copy()
             in_ls[i] = self.x_range[i]
-            u = self.Brent(in_ls, i)
+            u = self.Brent(in_ls, i, tol_Brent, tol_gr)
             out_ls.append(u)
         return out_ls, self.my_function(out_ls)
+
+    def mod_Powell(self):
+        iter_Powell = 1000
+        tol_Powell = 1
+        tol_Brent = 0.11
+        tol_gr = 0.11
+        x_init = self.x_0.copy()
+        f_init = self.my_function(x_init)
+        mat = self.mat.copy()
+        for j in range(iter_Powell):
+            print('Loop')
+            print(j)
+            x_last = x_init.copy()
+            f_x_last = f_init
+            delta = 0
+            for k in range(0, len(mat)):
+                mat_x = mat[k].copy()
+                minimized = self.min_line_search(x_0=[x + y for x, y in zip(x_last, mat_x)], tol_Brent=tol_Brent, tol_gr=tol_gr)
+                x, f_x = minimized
+                if f_x_last - f_x > delta:
+                    delta = f_x_last - f_x
+            f_x_last = f_x
+            x_last = x
+            print("Best x is ...")
+            print(x_last)
+            if f_x_last - f_init > tol_Powell:
+                print("Current Diff is")
+                print(f_x_last - f_init)
+                pass
+            else:
+                print("Initial f(x) is ...")
+                print(f_init)
+                print("Best f(x) is ...")
+                print(f_x_last)
+                return x, self.my_function(x)
+            f_extra = self.my_function([2 * x - y for x, y in zip(x_last, x_init)])
+            check_1 = f_extra >= f_init
+            check_2 = (f_init - 2 * f_x_last + f_extra) * (f_init - f_x_last - delta) ** 2 >= delta * (
+                        f_init - f_extra) ** 2
+            if check_1 or check_2:
+                x_init = x_last
+                tol_gr = tol_gr - 0.0001
+                tol_Brent = tol_Brent - 0.0001
+            else:
+                epsilon = [x - y for x, y in zip(x_last, x_init)]
+                minimized = self.min_line_search(x_0=[x + y for x, y in zip(x_last, epsilon)], tol_Brent=tol_Brent, tol_gr=tol_gr)
+                x, f_x = minimized
+                x_lambda = [(x - y) / z for x, y, z in zip(x, x_last, epsilon)]
+                mat_temp = mat.tolist()
+                mat_temp.append(x_lambda)
+                mat = mat_temp
+                x_init = x
+        print('Modified Powell Method Cannot Converge Within the Designated Iteration!')
+
 
 if __name__ == "__main__":
     def my_function(x_0):
@@ -105,7 +161,7 @@ if __name__ == "__main__":
         return 8 * y - 3
     x_0 = [0, 0]
     x_range = np.array([[-1, 1], [-1, 1]])
-    print(optim_non_drvt(x_0=x_0, x_range=x_range, my_function=my_function).min_line_search())
+    print(optim_non_drvt(x_0=x_0, x_range=x_range, my_function=my_function, mat='NA').min_line_search(x_0=x_0, tol_Brent=0.001, tol_gr=0.001))
 
     # Compare result with scipy
     res = minimize(my_function, x_0, bounds=x_range, jac=[dx_my_function, dy_my_function])
--- param.py
+++ param.py
@@ -5,6 +5,12 @@ from scipy.optimize import minimize
 from scipy.stats import norm
 import pandas as pd
 
+def safe_sqrt(x):
+    if x >=0:
+        return np.sqrt(x)
+    else:
+        return 0
+
 class GBM_param:
     def __init__(self, time_step, data):
         self.time_step = time_step
@@ -31,7 +37,8 @@ class GBM_param:
         # bds = [(0.001, 1), (0.001, 1)]
         bds = np.array([[0.001, 1], [0.001, 1]])
         # result = minimize(self.likelihood, par, bounds=bds)
-        result = optim.optim_non_drvt(x_0=par, x_range=bds, my_function=self.likelihood).min_line_search()
+        result = optim.optim_non_drvt(x_0=par, x_range=bds, my_function=self.likelihood, mat='NA').min_line_search(x_0=par, tol_Brent=0.001, tol_gr=0.001)
+        # result = optim.optim_non_drvt(x_0=par, x_range=bds, my_function=self.likelihood).mod_Powell()
         # return result.x
         return result[0]
 
@@ -50,11 +57,11 @@ class GBMSN_param:
         miu, sigma, delta, cro, Z_0 = par
         time = np.arange(self.dt(), self.T-self.t, self.dt())
         # Mean and Var for GBM + SN computed by Egami and Kevkhishvili
-        mean = (miu - 0.5 * sigma ** 2) * self.dt() + np.exp(-delta*time)*np.sqrt(cro*0.5/delta)*Z_0*(1-np.exp(delta*self.dt()))
-        var = (sigma ** 2) * self.dt() + (cro/(2*delta))*(1-np.exp(-2*delta*self.dt()))-((2*sigma*np.sqrt(cro))/delta)*(1-np.exp(-delta*self.dt()))*self.k_s
-        sigma_shot = np.sqrt(sigma**2+cro-2*sigma*np.sqrt(cro)*self.k_s)
+        mean = (miu - 0.5 * sigma ** 2) * self.dt() + np.exp(-delta*time)*safe_sqrt(cro*0.5/delta)*Z_0*(1-np.exp(delta*self.dt()))
+        var = (sigma ** 2) * self.dt() + (cro/(2*delta))*(1-np.exp(-2*delta*self.dt()))-((2*sigma*safe_sqrt(cro))/delta)*(1-np.exp(-delta*self.dt()))*self.k_s
+        sigma_shot = np.sqrt(sigma**2+cro-2*sigma*safe_sqrt(cro)*self.k_s)
         V_hat = tool.fixed_pt_iter_BS(self.data[0], self.data[1], sigma_shot, T = len(self.data[0])/self.time_step)
-        d_hat = ((np.log(V_hat)/self.data[1]+(sigma**2+cro-2*sigma*np.sqrt(cro)*self.k_s))*self.T/2)/(np.sqrt(self.T)*np.sqrt(sigma**2+cro-2*sigma*np.sqrt(cro)*self.k_s))
+        d_hat = ((np.log(V_hat)/self.data[1]+(sigma**2+cro-2*sigma*safe_sqrt(cro)*self.k_s))*self.T/2)/(np.sqrt(self.T)*np.sqrt(sigma**2+cro-2*sigma*safe_sqrt(cro)*self.k_s))
         n = len(self.data[0])-1
         A = np.sum(0.5*(np.log(V_hat[1:]/V_hat[:-1])-mean)**2/var)
         B = (n/2)*np.log(var)
@@ -64,13 +71,26 @@ class GBMSN_param:
         return llh
 
     def optim(self):
-        par = [0.03, 0.01, 0.5, 0.01, 0]
-        bds = [(0.01, 0.1), (0.001, 0.3), (0.001, 5), (0.001, 0.2), (-5, 5)]
-        #bds = np.array([[0.001, 0.5], [0.001, 1], [0.001, 5], [-5, 5], [-5, 5]])
-        result = minimize(self.likelihood, par, bounds=bds, method='Powell')
-        #result = optim.optim_non_drvt(x_0=par, x_range=bds, my_function=self.likelihood).min_line_search()
-        return result.x
-        #return result
+        # par = [0.03, 0.01, 0.5, 0.01, 0]
+        # par = [0.02, 0.2, 2, 0.5, 0]
+        # bds = [(0.01, 0.1), (0.001, 0.3), (0.001, 5), (0.001, 0.2), (-5, 5)]
+        bds = np.array([[0.001, 0.05], [0.001, 0.3], [0.001, 10], [0.001, 1], [-10, 10]])
+        par = np.mean(bds,axis=1)
+        n_vector = 10
+        e_list_list = []
+        for j in range(len(par)):
+            incr = (par[j] - bds[j][0]) / n_vector
+            e_list = []
+            for i in range(1, n_vector + 1):
+                e_list.append(i * incr)
+                e_list.append(-i * incr)
+            e_list_list.append(e_list)
+        e_input = np.array([[x[i] for x in e_list_list] for i in range(len(e_list_list[0]))])
+        # result = minimize(self.likelihood, par, bounds=bds, method='Powell')
+        # result = optim.optim_non_drvt(x_0=par, x_range=bds, my_function=self.likelihood).min_line_search(x_0=x_0)
+        result = optim.optim_non_drvt(x_0=par, x_range=bds, my_function=self.likelihood, mat=e_input).mod_Powell()
+        #return result.x
+        return result[0]
 
 if __name__ == "__main__":
     df_ED = pd.read_excel(r'Data\df_ED.xlsx')
